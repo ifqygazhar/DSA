3bd5066d83c5a856f08197c3d25a251553c8afcb
ifqygazhar <ifqygazhar@gmail.com>
2025-10-23 15:08:19 +0700

    feat: implement shift,deletebyindex and learning kind of reindex the data also fix type in get method from T into int

--- custom_list.dart
+++ custom_list.dart
@@ -12,7 +12,7 @@ class MyList<T> {
     this.length += 1;
   }
 
-  dynamic get(T indexPosition) {
+  dynamic get(int indexPosition) {
     return this.data[indexPosition];
   }
 
@@ -25,6 +25,45 @@ class MyList<T> {
       this.data.remove(lastIndex);
       this.length -= 1;
       return lastItem;
+    }
+  }
+
+  dynamic shift() {
+    if (this.length < 1) {
+      return null;
+    } else {
+      final firstItem = this.data[0];
+
+      // reindex
+      for (int i = 0; i < this.length; i++) {
+        final move = i + 1;
+        this.data[i] = this.data[move] as T;
+      }
+
+      // remove last item
+      this.data.remove(this.length - 1);
+      this.length -= 1;
+
+      return firstItem;
+    }
+  }
+
+  dynamic deleteByIndex(int indexPosition) {
+    if (indexPosition < 0 || indexPosition >= this.length) {
+      return null;
+    } else {
+      final item = this.data[indexPosition];
+
+      //reindex
+      for (int i = indexPosition; i < this.length; i++) {
+        final move = i + 1;
+        this.data[i] = this.data[move] as T;
+      }
+
+      // remove last item
+      this.data.remove(this.data.length - 1);
+      this.length -= 1;
+      return item;
     }
   }
 
@@ -43,4 +82,8 @@ void main() {
   print("get item ${myList.get(0)}");
   print("pop item ${myList.pop()}");
   print("after pop : ${myList}");
+  print("shift item ${myList.shift()}");
+  print("after shift : ${myList}");
+  print("delete by index: ${myList.deleteByIndex(0)}");
+  print("after delete by index : ${myList}");
 }
